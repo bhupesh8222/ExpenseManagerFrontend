e5945be49a93b835188367fe63b7619c3a7672e6
Bhupesh <bhupeshbhatt8222@gmail.com>
2023-04-17 22:33:46 +0530

    backend URL add

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:3000/api/v1/";
+const BASE_URL = "https://expense-manager-backend-nine.vercel.app/api/v1/";
 
 
 const GlobalContext = React.createContext()
